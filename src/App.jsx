9785214d21e306b808cc8306fe2f43f562b66c01
HARISHVANITHA <harishkumar2402005@gmail.com>
2025-03-27 19:44:54 +0530

    added new  file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import About from './components/About';
 import Skill from './components/Skill';
 import Contact from './components/Contact';
-function App() {
+import Content from './components/Content';
+function App() {  
   return (
       <div>
         <About/>
         <Skill/>
         <Contact/>
+        <Content/>
       </div>   
     
   );  
---
+++ src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./css/Content.css";
+function Content()
+{
+    return (
+        <div>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus pariatur eaque ab? Iste saepe vero necessitatibus asperiores pariatur ipsum eaque ad quo officia enim. Harum, repellendus? Rem veritatis ab blanditiis amet sit corrupti vel optio inventore accusantium vitae, quasi voluptatibus. Officia fugiat quae, quisquam voluptatibus consequatur, impedit perferendis modi et ut ipsa sed at dolorum fuga sint nisi pariatur totam eaque debitis minus harum vitae reiciendis recusandae. Voluptatum, eveniet harum. Eligendi aperiam magnam, obcaecati debitis ad nemo ut incidunt facere vel error iste eaque ea nobis reiciendis enim voluptates? Ut quae est non, assumenda suscipit omnis perferendis commodi saepe reprehenderit?
+
+           </p>
+        </div>
+    );
+}
+export default Content;
